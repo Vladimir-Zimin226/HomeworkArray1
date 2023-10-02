c087c968f9b8192d3d720860a3f0792956f06934
Владимир <100162794+Vladimir-Korolev555@users.noreply.github.com>
2023-10-03 00:56:09 +1100

    Homework completed!

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,46 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        int[] weights = {90, 91, 93, 97, 98, 95, 87, 86, 85, 0, 0, 0};
-        weights[0] = 90;
-        int januaryWeight = weights[0];
-        System.out.println(januaryWeight);
-        System.out.println(weights[0]);
-        System.out.println(weights[4]);
-        int january = 0;
-        System.out.println(weights[january]);
-        for (int i = 0; i < weights.length; i++) {
-            System.out.println(weights[i]);
+        //Task 1
+        int[] avengers = new int[3];
+        avengers[0] = 1;
+        avengers[1] = 2;
+        avengers[2] = 3;
+
+        double[] justiceLeague = {1.57, 7.654, 9.986};
+
+        String[] boys = new String[5];
+        boys[0] = "Бутчер";
+        boys[1] = "Хьюи";
+        boys[2] = "Француз";
+        boys[3] = "Кимико";
+        boys[4] = "Старлайт";
+
+        //Task 2
+        System.out.println(Arrays.toString(avengers));
+        System.out.println(Arrays.toString(justiceLeague));
+        System.out.println(Arrays.toString(boys));
+
+        //Task 3
+        for (int index = 2; index > -1; index--) {
+            System.out.print(avengers[index] + ", ");
         }
-        int[] arr = new int[10];
-        System.out.println(Arrays.toString(arr));
+        System.out.println();
+        for (int index = 2; index > -1; index--) {
+            System.out.print(justiceLeague[index] + ", ");
+        }
+        System.out.println();
+        for (int index = 4; index > -1; index--) {
+            System.out.print(boys[index] + ", ");
+        }
+        System.out.println();
+
+        //Task 4
+        for (int index = 0; index < avengers.length; index++) {
+            if (index % 2 != 1) {
+                avengers[index]++;
+            }
+        }
+        System.out.println(Arrays.toString(avengers));
     }
 }
